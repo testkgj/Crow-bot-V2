ae10b600c7f05f2fc7294d409d23a05523e3c4c4
testkgj <minecraftpaladium150410@gmail.com>
2025-04-19 16:58:14 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+TOKEN = "MTM2MTQxODQ2MzkzOTM5OTkwMQ.GVeXb8.aaqWdl-WUv8alnXI_kIGQuJYe_YPGcbn6MWgIg"
+PREFIX = "!"
+LOG_CHANNEL_ID = 1359250950409162993  # ID du salon de logs
+SUPPORT_ROLE_ID = 1361731825076273312  # ID du rôle
+MUTE_ROLE_ID = 1361774558155247897    # ID du rôle mute (à remplacer par le vrai)
+COMMAND_ROLE_ID = 1361428569959436308  # ✅ ID du rôle autorisé à exécuter certaines commandes comme lock/unlock
